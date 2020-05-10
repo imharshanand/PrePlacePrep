0de2b478af202f987ebcff7f5d94535b187f85d5
Harsh Anand <harsh19anand@gmail.com>
2020-05-10 23:28:19 +0530

    Initial Commit

--- src/Arrays/KadaneAlgorithm.java
+++ src/Arrays/KadaneAlgorithm.java
@@ -11,14 +11,13 @@ public class KadaneAlgorithm
     public static void main (String[] args) {
         Scanner s=new Scanner(System.in);
         int t=s.nextInt();
-        while(t>0){
+        while(t-->0){
             int n=s.nextInt();
             int[]a=new int[n];
             for(int i=0;i<n;i++){
                 a[i]=s.nextInt();
             }
             System.out.println(compute(a,n));
-            t--;
         }
     }
     public static int compute(int[]a,int n){
