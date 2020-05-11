8b8950a0b53a4aa36955e671c85dc79eca3c5acb
Harsh Anand <harsh19anand@gmail.com>
2020-05-11 22:33:42 +0530

    Initial Commit

--- src/String/Permutation.java
+++ src/String/Permutation.java
@@ -32,11 +32,9 @@ public class Permutation
             System.out.print(prefix+" ");
         }
         else
+        for(int i=0;i<n;i++)
         {
-            for(int i=0;i<n;i++)
-            {
-                permutation(prefix+str.charAt(i), str.substring(0,i)+str.substring(i+1,n));
-            }
+            permutation(prefix+str.charAt(i), str.substring(0,i)+str.substring(i+1,n));
         }
     }
 }
