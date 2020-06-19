ab45559e10531ad0dd09c3bfd2df4c1b4ac13159
Harsh Anand <harsh19anand@gmail.com>
2020-06-19 22:19:13 +0530

    Initial Commit

--- src/BitMagic/BitConversion.java
+++ src/BitMagic/BitConversion.java
@@ -2,16 +2,14 @@ package BitMagic;
 
 public class BitConversion {
     public static void main(String[] args) {
-        String number = "555"; // Number
+        String number = "5"; // Number
         int sBase = 10; // Source Base Octal
         int dBase = 2; // Destination Base Decimal
-        System.out.println("Octal to Decimal: "+ baseConversion(number, sBase, dBase));
-        dBase = 16; // Destination Base Hexadecimal
-        System.out.println("Octal to Hex: "+ baseConversion(number, sBase, dBase));
+        System.out.println("Source 10 to Dest 2: "+ baseConversion(number, sBase, dBase));
     }
     public static String baseConversion(String number, int sBase, int dBase)
     {
-        System.out.println("Source Base: "+Integer.parseInt(number, sBase));
-        return Integer.toString( Integer.parseInt(number, sBase),dBase );
+        System.out.println("Source with Base 10: "+Integer.parseInt(number, sBase));
+        return Integer.toString( Integer.parseInt(number),dBase );
     }
 }
