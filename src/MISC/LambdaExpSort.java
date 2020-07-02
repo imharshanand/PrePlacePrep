b56db95df2db8f24a0902018703a618de429b11e
Harsh Anand <harsh19anand@gmail.com>
2020-07-02 23:47:16 +0530

    Initial Commit

--- src/MISC/LambdaExpSort.java
+++ src/MISC/LambdaExpSort.java
@@ -1,12 +1,16 @@
 package MISC;
 import java.util.Arrays;
-public class LambdaExp
+public class LambdaExpSort
 {
     public static void main(String[] args) {
         int [][] arr = {{3,4},{1,2},{7,10},{10,5},{8,3}};
-        //Arrays.sort(arr, (a,b) -> a[0] < b[0] ? a[]);
-        System.out.println(Arrays.toString(arr));
+
         Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        //[1, 2] , [3, 4] , [7, 10] , [8, 3] , [10, 5]
+
+        //Arrays.sort(arr, (a, b) -> a[1] - b[1]);
+        //[1, 2] , [8, 3] , [3, 4] , [10, 5] , [7, 10]
+
         System.out.println(Arrays.toString(arr[0]));
         System.out.println(Arrays.toString(arr[1]));
         System.out.println(Arrays.toString(arr[2]));
