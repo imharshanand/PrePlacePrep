88dece369d65dd73f2d6dd9b62530181696f2e9f
Harsh Anand <harsh19anand@gmail.com>
2020-07-11 22:57:51 +0530

    Levelled down languageLevel="JDK_14" to languageLevel="JDK_1_8" & project-jdk-name="1.8"

--- src/StackQueue/DeQueueTest.java
+++ src/StackQueue/DeQueueTest.java
@@ -1,18 +1,36 @@
 package StackQueue;
 
 
+import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class DeQueueTest {
     public static void main(String[] args) {
-        Deque<Integer> dq = new LinkedList<>();
+//        Deque<Integer> dq = new LinkedList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+        System.out.println(dq.poll());
         dq.add(3);
         dq.push(4);
         dq.offer(6);
         dq.addFirst(2);
         dq.addLast(5);
         dq.addFirst(1);
+        dq.add(11);
+        System.out.println(dq.getFirst());
+        System.out.println(dq.contains(11));
+
+        System.out.println("ArrayDeque: " + dq.toString());
+//        Iterator it = dq.iterator();
+//        while(it.hasNext())
+//            System.out.print(it.next());
+
+//        System.out.println(dq.poll()); //Returning null + removing operations
+//        System.out.println("ArrayDeque: " + dq.toString());
+//        System.out.println(dq.pop()); //Throwing exception + removing operations
+//        System.out.println("ArrayDeque: " + dq.toString());
+        System.out.println(dq.remove());
         System.out.println("ArrayDeque: " + dq.toString());
     }
 }
