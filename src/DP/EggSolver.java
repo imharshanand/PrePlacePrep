4d61cb5a46891f2bfa28b7827b806a4c7dfe5df6
Harsh Anand <harsh19anand@gmail.com>
2020-07-15 22:24:16 +0530

    Initial Commit

--- src/DP/EggSolver.java
+++ src/DP/EggSolver.java
@@ -67,3 +67,18 @@ Time Complexity: O(n*k^2).
 Where ‘n’ is the number of eggs and ‘k’ is the number of floors,
 as we use a nested for loop ‘k^2’ times for each egg
  */
+
+/*
+public int superEggDrop(int K, int N)
+{
+    int[][] dp = new int[N + 1][K + 1];
+    int m = 0;
+    while (dp[m][K] < N)
+    {
+        ++m;
+        for (int k = 1; k <= K; ++k)
+            dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1;
+    }
+    return m;
+}
+ */
